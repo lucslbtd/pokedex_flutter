254a57d5b5b52b2420ac0c86020e55f787ec5218
Larissa <larissa.cerqueira.ribeiro@gmail.com>
2023-08-09 04:49:26 -0300

    change color scheme from app

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Pokedex',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromRGBO(23, 62, 165, 1)),
         useMaterial3: true,
       ),
       home: const SplashScreen(),
