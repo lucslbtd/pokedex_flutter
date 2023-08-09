c70da587a0e6198dae8d0c5713d981504e2315b6
Larissa <larissa.cerqueira.ribeiro@gmail.com>
2023-08-09 01:18:04 -0300

    create signup succes and username page

---
+++ lib/screens/signup_success.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex_flutter/screens/main_page.dart';
+
+class SignupSuccess extends StatelessWidget {
+  const SignupSuccess({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+            child: Column(
+          children: [
+            imgLoginSuccess(),
+            textSignupSuccess(),
+            Material(
+              elevation: 5.0,
+              color: const Color.fromARGB(255, 0, 26, 255),
+              borderRadius: BorderRadius.circular(30.0),
+              child: MaterialButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const MainPage()),
+                  );
+                },
+                minWidth: 200.0,
+                height: 42.0,
+                child: const Text(
+                  'Continuar',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            )
+          ],
+        )));
+  }
+}
+
+Widget imgLoginSuccess() {
+  return Expanded(
+    flex: 7,
+    child: Image.asset('assets/images/signup_success.png'),
+  );
+}
+
+Widget titleSignupSuccess() {
+  return Container(
+    width: 250,
+    color: Colors.white,
+    child: const Column(
+      mainAxisAlignment: MainAxisAlignment.center, // Align children in the center
+      children: [
+        Text(
+          'Sua conta foi criada com sucesso!',
+          style: TextStyle(
+            decoration: TextDecoration.none,
+            fontWeight: FontWeight.bold,
+            fontSize: 26,
+            color: Colors.black,
+          ),
+          textAlign: TextAlign.center, // Set text alignment to center
+        ),
+      ],
+    ),
+  );
+}
+
+
+Widget textSignupSuccess() {
+  return Expanded(
+    flex: 3,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center, // Align children in the center
+      children: [
+        titleSignupSuccess(),
+        const SizedBox(
+          height: 20,
+        ),
+        const Center(
+          child: SizedBox(
+            width: 300,
+            child: Text(
+              'Seja bem-vindo, treinador! Estamos animados para acompanhar sua jornada.',
+              textAlign: TextAlign.center, // Set text alignment to center
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
---
+++ lib/screens/username_page.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex_flutter/screens/signup_success.dart';
+
+class UsernamePage extends StatefulWidget {
+  const UsernamePage({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _UsernamePageState createState() => _UsernamePageState();
+}
+
+class _UsernamePageState extends State<UsernamePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: const Text(
+          'Criar conta',
+          style: TextStyle(
+            fontFamily: 'Poppins',
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Pra finalizar',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 26,
+                color: Colors.black,
+              ),
+            ),
+            const Text(
+              'Qual seu nome de usuário?',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 48),
+            const TextField(
+              decoration: InputDecoration(
+                hintText: 'cronasky',
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black, width: 1.0),
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black, width: 2.0),
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                ),
+              ),
+            ),
+            const SizedBox(height: 48),
+            Material(
+              elevation: 5.0,
+              color: const Color.fromARGB(255, 0, 26, 255),
+              borderRadius: BorderRadius.circular(30.0),
+              child: MaterialButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SignupSuccess()),
+                  );
+                },
+                minWidth: 200.0,
+                height: 42.0,
+                child: const Text(
+                  'Continuar',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
