8cd34cee7ea285fb5702e67eed0ecc535a5c9b96
Larissa <larissa.cerqueira.ribeiro@gmail.com>
2023-08-10 10:11:36 -0300

    start profile page

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,17 +1,113 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatefulWidget {
-  const ProfilePage({super.key});
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
 
-  @override
-  State<ProfilePage> createState() => _ProfilePageState();
-}
-
-class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Perfil'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profile"),
+      ),
+      backgroundColor: const Color(0xfff6f6f6),
+      body: Center(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 400),
+          child: ListView(
+            children: [
+              _SingleSection(
+                title: "Informações da conta",
+                children: [
+                  const _CustomListTile(
+                      title: "Nome",
+                      icon: CupertinoIcons.device_phone_portrait),
+                  _CustomListTile(
+                      title: "Email",
+                      icon: CupertinoIcons.moon,
+                      trailing:
+                          CupertinoSwitch(value: false, onChanged: (value) {})),
+                  const _CustomListTile(
+                      title: "Senha",
+                      icon: CupertinoIcons.cloud_download),
+                ],
+              ),
+              const _SingleSection(
+                title: "Pokédex",
+                children: [
+  
+                ],
+              ),
+               _SingleSection(
+                title: "Notificações",
+                children: [
+                  _CustomListTile(
+                      title: "Atualizações na pokédex", icon: CupertinoIcons.person_2, trailing:
+                          CupertinoSwitch(value: false, onChanged: (value) {})),
+                  _CustomListTile(
+                      title: "Mundo Pokémon", icon: CupertinoIcons.lock, trailing:
+                          CupertinoSwitch(value: false, onChanged: (value) {})),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final Widget? trailing;
+  const _CustomListTile(
+      {Key? key, required this.title, required this.icon, this.trailing})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      leading: Icon(icon),
+      trailing: trailing ?? const Icon(CupertinoIcons.forward, size: 18),
+      onTap: () {},
+    );
+  }
+}
+
+class _SingleSection extends StatelessWidget {
+  final String title;
+  final List<Widget> children;
+  const _SingleSection({
+    Key? key,
+    required this.title,
+    required this.children,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 16),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            title.toUpperCase(),
+            style:
+                Theme.of(context).textTheme.headline3?.copyWith(fontSize: 16),
+          ),
+        ),
+        Container(
+          width: double.infinity,
+          color: Colors.white,
+          child: Column(
+            children: children,
+          ),
+        ),
+      ],
     );
   }
 }
